d65e32a912c0390a6e1c36ca8e6c71ff6108a1d7
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-26 20:33:10 +0200

    Notifications Logic

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,4 +1,30 @@
 "use strict";
-const msg = 'Typescript compilation is onnnn.';
-console.log(msg);
+const notificationsAmountEl = document.getElementById('notification-amount');
+const notificationsResetEl = document.getElementById('notification-reset');
+const notificationListEl = document.querySelectorAll('ul li');
+const notificationArray = [];
+const getNotificationAmount = (list) => {
+    list.forEach((item) => {
+        if (item.classList.contains('new')) {
+            notificationArray.push(item);
+        }
+    });
+    const amount = notificationArray.length.toString();
+    notificationsAmountEl.textContent = amount;
+};
+const checkNotifications = (list) => {
+    list.forEach((item, index) => {
+        if (item.classList.contains('new')) {
+            notificationArray.splice(index);
+            item.classList.remove('new');
+        }
+    });
+    const amount = notificationArray.length.toString();
+    notificationsAmountEl.textContent = amount;
+};
+const initApp = () => {
+    getNotificationAmount(notificationListEl);
+    notificationsResetEl.addEventListener('click', () => checkNotifications(notificationListEl));
+};
+document.addEventListener('DOMContentLoaded', initApp);
 //# sourceMappingURL=index.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,2 +1,37 @@
-const msg = 'Typescript compilation is onnnn.';
-console.log(msg);
+const notificationsAmountEl = document.getElementById('notification-amount') as HTMLDivElement;
+const notificationsResetEl = document.getElementById('notification-reset') as HTMLButtonElement;
+const notificationListEl = document.querySelectorAll('ul li');
+
+const notificationArray: Element[] = [];
+
+const getNotificationAmount = (list: NodeListOf<Element>): void => {
+	list.forEach((item) => {
+		if (item.classList.contains('new')) {
+			notificationArray.push(item);
+		}
+	});
+
+	const amount = notificationArray.length.toString();
+
+	notificationsAmountEl.textContent = amount;
+};
+
+const checkNotifications = (list: NodeListOf<Element>): void => {
+	list.forEach((item, index) => {
+		if (item.classList.contains('new')) {
+			notificationArray.splice(index);
+			item.classList.remove('new');
+		}
+	});
+
+	const amount = notificationArray.length.toString();
+
+	notificationsAmountEl.textContent = amount;
+};
+
+const initApp = () => {
+	getNotificationAmount(notificationListEl);
+	notificationsResetEl.addEventListener('click', () => checkNotifications(notificationListEl));
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
